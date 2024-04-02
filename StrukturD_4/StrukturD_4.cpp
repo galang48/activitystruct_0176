80b052fb6770d7147dc22a39aa0552108135147a
galang48 <galangyudha1945@gmail.com>
2024-04-02 12:46:28 +0700

    memanggil semuaa

--- StrukturD_4/StrukturD_4.cpp
+++ StrukturD_4/StrukturD_4.cpp
@@ -17,7 +17,7 @@ struct Mahasiswa {
 int main(){
     Mahasiswa mhs[3];
     for (int i = 0; i < 3; i++) {
-        cout << "Data ke-" << (i + i) << ":" << endl;
+        cout << "Data ke-" << (i + 1) << ":" << endl;
         cout << "Nomor Mahasiswa : ";
         getline(cin, mhs[i].nim);
         cout << "Nama Mahasiswa : ";
@@ -31,6 +31,17 @@ int main(){
         cout << "Umur Mahasiswa : ";
         cin >> mhs[i].umur;
         cin.ignore(1, '\n');
+        cout << endl;
+    }
+    for (int i = 0; i < 3; i++) {
+        cout << endl;
+        cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
+        cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t desa : " << mhs[i].alamat.desa;
+        cout << "\n \t desa : " << mhs[i].alamat.kota;
+        cout << "\n Umur : " << mhs[i].umur;
         cout << endl;
     }
 }
