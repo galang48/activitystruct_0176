e6b4b76bf5f591f3460ff76b4c31f86270a76d3b
galang48 <galangyudha1945@gmail.com>
2024-04-02 10:52:35 +0700

    menambahkan nama desa

--- StrukturD_2/StrukturD_2.cpp
+++ StrukturD_2/StrukturD_2.cpp
@@ -19,6 +19,10 @@ int main(){
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
 }
 
 
